757eee4a07357303ad3cf803a649988392ebae71
Leo <leo.lll@gmail.com>
2023-03-20 21:55:56 -0700

    Styling wip

--- resources/views/components/action-section.blade.php
+++ resources/views/components/action-section.blade.php
@@ -1,7 +1,7 @@
 @props(['title', 'description'])
 <div {{ $attributes->class(['grid grid-cols-1 md:grid-cols-3 gap-6 filament-companies::action-section']) }}>
     <div class="col-span-1 flex justify-between">
-        <div class="px-4 sm:px-0">
+        <div>
             <h3 @class([
                 'text-lg font-medium text-gray-900 filament-companies::action-title',
                 'dark:text-white' => config('filament.dark_mode'),
